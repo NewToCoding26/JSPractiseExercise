bdbd0151c29f40a3618abe1405aa2eaabb02497d
NewToCoding26 <liridon992@gmx.de>
2025-02-12 14:10:06 +0100

    nicht weiter gekommen

--- ZehnteAufgabe.js
+++ ZehnteAufgabe.js
@@ -1,4 +1,13 @@
         /* Write a JavaScript program to check a given string contains
         2 to 4 occurrences of a specified character. */
 
-const 
+const countChars = (str, char) => str.split('').filter(ch => ch === char).length;
+
+const contains2To4 = (str, char) => countChars(str, char) >= 2 && countChars (str, char) <= 4;
+
+console.log(contains2To4('ooh!', 'o'))
+console.log(contains2To4('oh!', 'o'))
+console.log(contains2To4('ooooh!', 'o'))
+console.log(contains2To4('oooh!', 'o'))
+console.log(contains2To4('oooooooh!', 'o'))
+console.log(contains2To4('ooh!', 'o'))
