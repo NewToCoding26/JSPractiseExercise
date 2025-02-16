9b3c97eee3e410589967b0df11be035b7444463d
NewToCoding26 <liridon992@gmx.de>
2025-02-16 10:45:55 +0100

    Bis Aufgabe 25

--- 25Aufgabe.js
+++ 25Aufgabe.js
@@ -9,6 +9,6 @@ const firstNotRepeatedCharacter = (str) =>
     str.split('')
         .filter((item, index, arr) => 
             arr.filter(arrItem => arrItem === item).length === 1
-        );
+        )[0];
 
-console.log(firstNotRepeatedCharacter('abbaccddeeache'));
+console.log(firstNotRepeatedCharacter('abaccdeeache'));
